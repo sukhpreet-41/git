76a04fd930c388d78293ab3a9348644915ab5bbf
sukhpreet-41 <73161820+sukhpreet-41@users.noreply.github.com>
2023-02-18 23:45:12 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,12 @@
 
 
 sudo snap install docker
+sudo chmod 666 /var/run/docker.sock
+
 sudo apt install ansible -y
+
+git clone https://github.com/sukhpreet-41/react-weather-app.git
+
+cd react-weather-app
+
+sudo docker-compose -f docker-compose-dev -f docker-compose-prod up -d --build
